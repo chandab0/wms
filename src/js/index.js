227f12198c4bd316c9fe4d1b3edcd4d6f5cc8b2e
Saptarshi Basu <sapb2004@gmail.com>
2018-06-10 12:26:30 +0530

    secondary heading added and mobile image added

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,17 @@
 import style from "../sass/styles.scss";
 import AOS from 'aos';
 
+
+/*window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+}*/
+
 AOS.init({
     once: true,
     disable: 'mobile'
