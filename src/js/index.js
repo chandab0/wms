c618f7b9d271342105097d32a84c0c731fb85474
Saptarshi Basu <sapb2004@gmail.com>
2018-06-11 20:16:20 +0530

    fine-tuning done in menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,22 @@
 import style from "../sass/styles.scss";
 import AOS from 'aos';
 
-
-/*window.onscroll = function() {scrollFunction()};
+document.addEventListener('DOMContentLoaded', function(e) {
+    window.onscroll = function () { scrollFunction() };
+    document.getElementsByClassName("footer__copyright").item(0).querySelector('p').innerHTML = 'Copyright &copy; ' + new Date().getFullYear() + ' World Marketing Summit';
+});
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-50px";
+    console.log(document.getElementById("navbar").classList.contains("c-navbar__sticky"));
+
+    if ((document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) && !document.getElementById("navbar").classList.contains("c-navbar__sticky")) {
+        document.getElementById("navbar").classList.add("c-navbar__sticky");
+    } else if (!(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) && document.getElementById("navbar").classList.contains("c-navbar__sticky")) {
+        document.getElementById("navbar").classList.remove("c-navbar__sticky");
     }
-}*/
+}
 
 AOS.init({
     once: true,
     disable: 'mobile'
 });
-document.getElementsByClassName("footer__copyright").item(0).querySelector('p').innerHTML = 'Copyright &copy; ' + new Date().getFullYear() + ' World Marketing Summit';
