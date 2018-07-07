5756547af5a0f5733f956a8f832e0406aa268794
Saptarshi Basu <sapb2004@gmail.com>
2018-07-07 15:28:51 +0530

    Samarkan font, schedule changes, some style changes in schedule are done

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|jpg|gif|svg)$/,
+                test: /\.(png|jpg|gif|svg|TTF)$/,
                 use: [
                     {
                         loader: 'file-loader',
