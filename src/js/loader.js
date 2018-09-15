936b00c010a5f8261bca3b1b3031415bbc5984a9
Saptarshi Basu <sapb2004@gmail.com>
2018-09-15 21:31:24 +0530

    minor fixes

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,6 +1,6 @@
 import loader from '../sass/loader.scss';
 var paceOptions = {
-    ajax: false, // disabled
+    ajax: true, // disabled
     document: true, // disabled
-    eventLag: false // disabled
+    eventLag: true // disabled
 };
