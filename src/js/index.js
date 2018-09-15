e7007324e7bc65b024ce1ff7572d085925bfeb4f
Saptarshi Basu <sapb2004@gmail.com>
2018-09-15 21:06:50 +0530

    Award page revamped

--- src/js/index.js
+++ src/js/index.js
@@ -68,7 +68,7 @@ $(function () {
     $('.same-page').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
+            scrollTop: $($(this).attr('href')).offset().top - 60
         }, 500, 'linear');
     });
 
