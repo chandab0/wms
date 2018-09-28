aac290c08d7a9193bcc8331247b2eea5c4412de5
Saptarshi Basu <sapb2004@gmail.com>
2018-09-28 08:57:44 +0530

    Faisal profile changed and timer added

--- src/js/index.js
+++ src/js/index.js
@@ -68,7 +68,7 @@ window.toggleHamburger = function () {
     }
 }
 
-window.closePopup = function() {
+window.closePopup = function () {
     $(".popup").toggleClass("popup__closed")
 }
 
@@ -88,13 +88,15 @@ window.closeMobMenu = function () {
     document.getElementsByClassName("c-navbar")[0].classList.remove("c-navbar__mobile-menu");
 }
 
-window.changeBorderColor = function(id) {
+window.changeBorderColor = function (id) {
     console.log("=========");
     $(".side-content__selected-border").toggleClass("side-content__selected-border");
     $(id).toggleClass("side-content__selected-border");
 };
 
 $(document).ready(function () {
+    timer();
+
     scrollFunction();
 
     $("#sponsorForm").submit(function (event) {
@@ -106,7 +108,7 @@ $(document).ready(function () {
                     to: "sponsors@wms18.com",
                     subject: $('#inputPkg').val(),
                     text: $('#inputName').val() + " | " + $('#inputEmail').val() + " | " + $('#inputContactNo').val() + " | " + $('#inputMsg').val(),
-                    html: "<h1>"+$('#inputPkg').val()+"</h1><p>"+$('#inputName').val()+"</p><p>"+$('#inputEmail').val()+"</p><p>"+$('#inputContactNo').val()+"</p><p>"+$('#inputMsg').val()+"</p>"
+                    html: "<h1>" + $('#inputPkg').val() + "</h1><p>" + $('#inputName').val() + "</p><p>" + $('#inputEmail').val() + "</p><p>" + $('#inputContactNo').val() + "</p><p>" + $('#inputMsg').val() + "</p>"
                 }
             },
             success: function (response) {
@@ -124,7 +126,7 @@ $(document).ready(function () {
     $(document).on("click", ".packages__pkg-header .c-btn", function () {
         var pkgName = $(this).data('pkg');
         $("#inputPkg").val(pkgName);
-   });
+    });
 
     $(".submit-button").click(function () {
         $("#sponsorForm").submit();
@@ -149,3 +151,36 @@ $('#sponsor-carousel').slick({
         }
     ]
 });
+
+window.timer = function () {
+    // Set the date we're counting down to
+    var countDownDate = new Date("Dec 14, 2018 08:30:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+        // Get todays date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+
+        // If the count down is finished, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+}
